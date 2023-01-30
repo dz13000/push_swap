593bb592e7ceb7e330846ae78bd1d64415fc47ec
dz13000 <kamilbop93@gmail.com>
2023-01-30 21:22:29 +0100

    ok2

--- algo/tri.c
+++ algo/tri.c
@@ -137,6 +137,63 @@ void	asign_cc1(t_pile **pile_a, t_pile **pile_b, t_pile **pos_a, t_pile **pos_b)
 	}
 }
 
+void	asign_cc2(t_pile **pile_a, t_pile **pile_b, t_pile **pos_a, t_pile **pos_b)
+{
+	t_pile *tmp;
+	t_pile *tmp2;
+	int i;
+	int j;
+	int	tt;
+	int temp;
+
+	tmp = (*pile_a);
+	tmp2 = (*pile_b);
+	i = 0;
+	j = 0;
+	tt = 0;
+	temp = 0;
+	while(tmp)
+	{
+		i++;
+		if (tmp == (*pos_a))
+			break;
+		tmp = tmp->next;
+	}
+	i--;
+	while(tmp2)
+	{
+		j++;
+		if (tmp2 == (*pos_b))
+			break;
+		tmp2 = tmp2->next;
+	}
+	j--;
+	if (i != 0)
+	{	
+		temp = ft_lst_size(&(*pile_a)) - i;
+		i = temp;
+	}
+	if (j != 0)
+	{	
+		temp = ft_lst_size(&(*pile_b)) - j;
+		j = temp;
+	}
+	(*pos_b)->up_b = 0;
+	(*pos_b)->up_a = 0;
+	(*pos_b)->down_a = i;
+	(*pos_b)->down_b = j;
+	if (j >= i)
+	{
+		tt = j - i;
+		(*pos_b)->total_coups = tt + i;
+	}
+	else
+	{
+		tt = i - j;
+		(*pos_b)->total_coups = tt + j;
+	}
+}
+
 void	calcul_prix(t_pile **pile_a, t_pile **pile_b)
 {
 	t_pile	*tmp;
@@ -144,6 +201,7 @@ void	calcul_prix(t_pile **pile_a, t_pile **pile_b)
 	t_cc	cc;
 
 	cc.cc1 = 0;
+	cc.cc2 = 0;
 	tmp2 = (*pile_b);
     while(tmp2)
     {
@@ -155,7 +213,7 @@ void	calcul_prix(t_pile **pile_a, t_pile **pile_b)
 				cc.cc1 = calcul_coups1(&(*pile_a), &(*pile_b), tmp, tmp2);
 				cc.cc2 = calcul_coups2(&(*pile_a), &(*pile_b), tmp, tmp2);
 				//if (cc.cc1 <= cc.cc2)
-					asign_cc1(&(*pile_a), &(*pile_b), (&tmp), (&tmp2));
+					asign_cc2(&(*pile_a), &(*pile_b), (&tmp), (&tmp2));
 				break;
 			}
 			tmp = tmp->next;
